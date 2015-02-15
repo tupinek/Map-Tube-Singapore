ce78fd0928e31ad05b239ecad4e28f419116efe7
Michal Špaček <tupinek@gmail.com>
2015-02-15 15:09:27 +0100

    Fill EXAMPLE1 output.

--- Singapore.pm
+++ Singapore.pm
@@ -122,7 +122,7 @@ For more information about Singapore Map, click L<here|https://en.wikipedia.org/
  print "Route: ".$route."\n";
 
  # Output:
- # TODO
+ # Route: Admiralty (North South MRT Line), Sembawang (North South MRT Line), Canberra (North South MRT Line), Yishun (North South MRT Line), Khatib (North South MRT Line), Yio Chu Kang (North South MRT Line), Ang Mo Kio (North South MRT Line), Bishan (North South MRT Line), Bishan (Circle MRT Line), Lorong Chuan (Circle MRT Line), Serangoon (Circle MRT Line), Bartley (Circle MRT Line), Tai Seng (Circle MRT Line), MacPherson (Circle MRT Line), Paya Lebar (Circle MRT Line), Paya Lebar (East West MRT Line), Eunos (East West MRT Line), Kembangan (East West MRT Line), Bedok (East West MRT Line), Tanah Merah (East West MRT Line), Simei (East West MRT Line), Tampines (East West MRT Line)
 
 =head1 EXAMPLE2
 
