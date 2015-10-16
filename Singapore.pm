5a9e0768aa70c09086eff871b780e89a7b528fdf
Michal Špaček <tupinek@gmail.com>
2015-10-16 09:52:09 +0200

    Add EXAMPLE4 to doc.

--- Singapore.pm
+++ Singapore.pm
@@ -194,6 +194,32 @@ For more information about Singapore Map, click L<here|https://en.wikipedia.org/
 
 =end html
 
+=head1 EXAMPLE4
+
+ # Pragmas.
+ use strict;
+ use warnings;
+
+ # Modules.
+ use Encode qw(encode_utf8);
+ use Map::Tube::Singapore;
+
+ # Object.
+ my $obj = Map::Tube::Singapore->new;
+
+ # Get lines.
+ my $lines_ar = $obj->get_lines;
+
+ # Print out.
+ map { print encode_utf8($_->name)."\n"; } sort @{$lines_ar};
+
+ # Output:
+ # Circle MRT Line
+ # Downtown MRT Line
+ # East West MRT Line
+ # North East MRT Line
+ # North South MRT Line
+
 =head1 DEPENDENCIES
 
 L<File::Share>,
