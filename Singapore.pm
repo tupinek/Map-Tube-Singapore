7e7bc33dfeb78b6a9b2c31c5fa22e21b90f847b1
Michal Špaček <tupinek@gmail.com>
2015-11-05 11:55:21 +0100

    Prepare for 0.04 version.

--- Singapore.pm
+++ Singapore.pm
@@ -11,7 +11,7 @@ use Moo;
 use namespace::clean;
 
 # Version.
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 # Get XML.
 has xml => (
@@ -324,6 +324,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
